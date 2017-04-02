4e2617cb4fc25f31d47c1bb040382dbee53d54d5
xlzd <i@xlzd.me>
2017-04-02 20:19:12 +0800

    version ++

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='img2html',
-    version='0.0.1',
+    version='0.0.2',
     author='xlzd',
     author_email='i@xlzd.me',
     description='Convert image to HTML',
