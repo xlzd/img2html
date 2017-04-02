3d956cdaf200409d526f6e1cc17d1a3a9dc12833
xlzd <i@xlzd.me>
2017-04-02 18:39:38 +0800

    set up

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='img2html',
+    version='0.0.1',
+    author='xlzd',
+    author_email='i@xlzd.me',
+    description='Convert image to HTML',
+    url='https://github.com/xlzd/img2html',
+    license='WTFPL',
+    packages=['img2html'],
+    install_requires=[
+        'jinja2',
+        'pillow'
+    ],
+    entry_points={
+        'console_scripts': [
+            'img2html=img2html:main'
+        ]
+    }
+)
